bae6975f4be7827d6fdd6d4c8b5878285421ab45
Emile Victor <emilevictor@gmail.com>
2012-08-24 17:00:45 +1000

    added stubs for new swing UI

---
+++ src/mentalpoker/SwingGUI.java
@@ -0,0 +1,16 @@
+package mentalpoker;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class SwingGUI extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
